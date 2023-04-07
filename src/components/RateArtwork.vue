19f09b175ccdaa099732492650c4fa80c9198526
bereket <bekhonelign@gmail.com>
2023-04-06 18:10:36 -0700

    vote artwork

--- src/components/RateArtwork.vue
+++ src/components/RateArtwork.vue
@@ -31,7 +31,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .star {
   display: inline-block;
   color: #f5c518;
---
+++ src/components/VoteForArtwork.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-body text-center">
+              <h5 class="card-title">{{ artwork.title }}</h5>
+              <img :src="artwork.imageUrl" class="img-fluid mb-3" alt="">
+              <p class="card-text">{{ artwork.description }}</p>
+              <button class="btn btn-primary" @click="vote()">Vote {{ artwork.votes }}</button>
+              <i class="fas fa-thumbs-up fa-2x ml-2"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    name: 'VoteForArtwork',
+    props: ['artwork'],
+    setup(props) {
+      const votes = ref(props.artwork.votes);
+  
+      const vote = () => {
+        votes.value++;
+      };
+  
+      return {
+        votes,
+        vote,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your custom styles here */
+  </style>
+  

ccf0e8a23c3b6e8ce1d4596952833df8e20ad516
bereket <bekhonelign@gmail.com>
2023-04-07 18:57:23 -0700

    update manager panel

--- src/views/Manager/ManagerPanel.vue
+++ src/views/Manager/ManagerPanel.vue
@@ -49,7 +49,7 @@
 <table class="table table-dark">
   <thead>
     <tr>
-      <th scope="col">#</th>
+      <th scope="col">Id</th>
       <th scope="col">First</th>
       <th scope="col">Last</th>
       <th scope="col">Handle</th>
@@ -89,7 +89,7 @@
   })
   </script>
   
-  <style>
+  <style scoped>
   /* add custom styles here */
   </style>
   

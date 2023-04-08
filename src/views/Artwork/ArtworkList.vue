6e866ad986831cfbdfa91d77c0dc6dc382e6d914
bereket <bekhonelign@gmail.com>
2023-04-07 19:05:57 -0700

    page item edit

--- src/views/Artwork/ArtworkList.vue
+++ src/views/Artwork/ArtworkList.vue
@@ -47,12 +47,12 @@
 
 </div>
 
-<nav aria-label="Page navigation example">
+<nav aria-label="Page navigation">
   <ul class="pagination justify-content-center">
     <li class="page-item disabled">
       <a class="page-link">Previous</a>
     </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item" ><a class="page-link" href="#">1</a></li>
     <li class="page-item"><a class="page-link" href="#">2</a></li>
     <li class="page-item"><a class="page-link" href="#">3</a></li>
     <li class="page-item"><a class="page-link" href="#">4</a></li>

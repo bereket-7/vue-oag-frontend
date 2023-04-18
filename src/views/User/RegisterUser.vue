1cfcaa941de3c7c16581625859b73c84577cf4db
bereket honelign <bekhonelign@gmail.com>
2023-04-18 17:28:16 +0300

    validation for sign up

--- src/views/User/RegisterUser.vue
+++ src/views/User/RegisterUser.vue
@@ -1,58 +1,82 @@
 <template>
-  <div class="container">
-    <h2>Register</h2>
-    <form @submit.prevent="submitForm">
-      <div class="mb-3">
-        <label for="firstname" class="form-label">First Name</label>
-        <input type="text" class="form-control" id="firstname" v-model="user.firstname" required>
-        <div class="invalid-feedback">Please provide a valid first name.</div>
-      </div>
-      <div class="mb-3">
-        <label for="lastname" class="form-label">Last Name</label>
-        <input type="text" class="form-control" id="lastname" v-model="user.lastname" required>
-        <div class="invalid-feedback">Please provide a valid last name.</div>
-      </div>
-      <div class="mb-3">
-        <label for="email" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="email" v-model="user.email" required>
-        <div class="invalid-feedback">Please provide a valid email address.</div>
-      </div>
-      <div class="mb-3">
-        <label for="phone" class="form-label">Phone</label>
-        <input type="tel" class="form-control" id="phone" v-model="user.phone" required>
-        <div class="invalid-feedback">Please provide a valid phone number.</div>
-      </div>
-      <div class="mb-3">
-        <label for="address" class="form-label">Address</label>
-        <input type="text" class="form-control" id="address" v-model="user.address" required>
-        <div class="invalid-feedback">Please provide a valid address.</div>
-      </div>
-      <div class="mb-3">
-        <label for="age" class="form-label">Age</label>
-        <input type="number" class="form-control" id="age" v-model="user.age" required>
-        <div class="invalid-feedback">Please provide a valid age.</div>
-      </div>
-      <div class="mb-3">
-        <label for="sex" class="form-label">Sex</label>
-        <select class="form-control" id="sex" v-model="user.sex" required>
-          <option value="">Select one</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-          <option value="other">Other</option>
-        </select>
-        <div class="invalid-feedback">Please select a valid sex.</div>
-      </div>
-      <div class="mb-3">
-        <label for="photo" class="form-label">Photo</label>
-        <input type="file" class="form-control" id="photo" v-on:change="handleFileUpload" required>
-        <div class="invalid-feedback">Please provide a photo.</div>
-      </div>
-      <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="password" v-model="user.password" required>
-        <div class="invalid-feedback">Please provide a valid password.</div>
-      </div>
-      <button class="btn btn-success" type="submit">submit</button>
-      </form>
-      </div>
-    </template>
+  <form>
+    <label for="firstname">First Name</label>
+    <input type="text" id="firstname" v-model="firstName" />
+    <span v-if="!firstName" class="error">Please enter your first name.</span>
+
+    <label for="lastname">Last Name</label>
+    <input type="text" id="lastname" v-model="lastName" />
+    <span v-if="!lastName" class="error">Please enter your last name.</span>
+
+    <label for="email">Email</label>
+    <input type="email" id="email" v-model="email" />
+    <span v-if="!email || !validEmail" class="error">Please enter a valid email address.</span>
+
+    <label for="phone">Phone</label>
+    <input type="text" id="phone" v-model="phone" />
+    <span v-if="!phone" class="error">Please enter your phone number.</span>
+
+    <label for="address">Address</label>
+    <input type="text" id="address" v-model="address" />
+    <span v-if="!address" class="error">Please enter your address.</span>
+
+    <label for="age">Age</label>
+    <input type="number" id="age" v-model="age" />
+    <span v-if="!age" class="error">Please enter your age.</span>
+
+    <label for="sex">Sex</label>
+    <select id="sex" v-model="sex">
+      <option value="male">Male</option>
+      <option value="female">Female</option>
+      <option value="other">Other</option>
+    </select>
+    <span v-if="!sex" class="error">Please select your gender.</span>
+
+    <label for="photo">Photo</label>
+    <input type="file" id="photo"  @change="onFileChange" />
+    <span v-if="!photo" class="error">Please upload a photo.</span>
+
+    <label for="password">Password</label>
+    <input type="password" id="password" v-model="password" />
+    <span v-if="!password" class="error">Please enter a password.</span>
+
+    <button type="submit" @click.prevent="submitForm">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      address: '',
+      age: '',
+      sex: '',
+      photo: null,
+      password: ''
+    }
+  },
+  computed: {
+    validEmail() {
+      // regular expression for email validation
+      const emailRegex = /\S+@\S+\.\S+/
+      return emailRegex.test(this.email)
+    }
+  },
+  methods: {
+    onFileChange(event) {
+      this.photo = event.target.files[0];
+    },
+    submitForm() {
+      if (!this.firstName || !this.lastName || !this.email || !this.validEmail || !this.phone || !this.address || !this.age || !this.sex || !this.photo || !this.password) {
+        alert('Please fill out all required fields.')
+      } else {
+        // submit form data to backend
+      }
+    }
+  }
+}
+</script>

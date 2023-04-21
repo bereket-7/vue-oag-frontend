130541af50e121581ab7d6848f82270d4fd686b0
bereket honelign <bekhonelign@gmail.com>
2023-04-21 07:50:54 +0300

    event display

---
+++ src/views/Organization/EventDisplay.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">content</p>
+  </div>
+</div>
+</template>

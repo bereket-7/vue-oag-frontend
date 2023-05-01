955f9b0ec59cd950bcec1758592c958f2a226a8e
bereket honelign <bekhonelign@gmail.com>
2023-05-01 20:38:14 +0300

    css style

--- src/views/Manager/CreateCompetition.vue
+++ src/views/Manager/CreateCompetition.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div class="form-container">
     <h1>Add Competition</h1>
-    <form @submit.prevent="addCompetition">
+    <form @submit.prevent="addCompetition"   class="form">
       <label for="title">Title:</label>
       <input type="text" id="title" v-model="competition.competitionTitle" required><br><br>
 
@@ -47,3 +47,66 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+    .form-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+.form input[type="text"],
+.form input[type="email"],
+.form input[type="password"],
+.form input[type="date"] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  box-sizing: border-box;
+  background-color: #f8f8f8;
+}
+
+.form input[type="text"]:hover,
+.form input[type="email"]:hover,
+.form input[type="password"]:hover,
+.form input[type="date"]:hover {
+  background-color: #e8e8e8;
+}
+
+.form button {
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  padding: 12px 20px;
+  margin: 8px 0;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.form button:hover {
+  background-color: #3e8e41;
+}
+
+.form button[type="reset"] {
+  background-color: #f44336;
+}
+
+.form button[type="reset"]:hover {
+  background-color: #d32f2f;
+}
+
+</style>

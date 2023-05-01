dea49e3595e796c2795c9dfafdca0d4729f1c1f4
bereket honelign <bekhonelign@gmail.com>
2023-05-01 08:21:41 +0300

    app vue

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
             </ul>
           </div>
         <li class="nav-item">
-          <router-link class="nav-link" to="/register">SignUp</router-link>
+          <router-link class="nav-link" to="/userSignup">SignUp</router-link>
         </li>
 
         <li class="nav-item">
@@ -54,8 +54,6 @@
 </nav>
   <router-view/>
 </template>
-
-
 
 <style>
 #app {

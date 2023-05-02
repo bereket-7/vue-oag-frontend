53ffd00afb38c36f0d9a50eb3819940b83b25e2a
bereket honelign <bekhonelign@gmail.com>
2023-05-02 14:43:01 +0300

    validation org register

--- src/views/Organization/RegisterOrganization.vue
+++ src/views/Organization/RegisterOrganization.vue
@@ -1,20 +1,29 @@
 <template>
   <br><br>
   <div class="form-container">
-    <h1>Organization Signup</h1>
+    <h1>Sign-up</h1>
     <hr class="mx-n3">
     <br>
     <form @submit.prevent="registerOrganization">
-      <label>Name:</label>
+      <label>Name</label>
       <input type="text" v-model="name" required />
-      <label>Email:</label>
+      <span v-if="errors.name" class="error">{{ errors.name }}</span>
+      <label>Email</label>
       <input type="email" v-model="email" required />
+      <span v-if="errors.email" class="error">{{ errors.email }}</span>
       <label>Phone:</label>
-      <input type="text" v-model="phone" required />
-      <label>Address:</label>
+      <input type="tel" v-model="phone" required />
+      <span v-if="errors.phone" class="error">{{ errors.phone }}</span>
+      <label>Address</label>
       <input type="text" v-model="address" required />
-      <label>Password:</label>
+      <label>Password</label>
       <input type="password" v-model="password" required />
+      <span v-if="errors.password" class="error">{{ errors.password }}</span>
+      <label>Confirm</label>
+      <input type="password" v-model="confirmPassword" required />
+      <span v-if="errors.confirmPassword" class="error">{{ errors.confirmPassword}}</span>
+      <p v-if="passwordError">{{ passwordError }}</p>
+      <div v-if="errorMessage" class="alert alert-danger">{{ errorMessage }}</div>
       <hr class="mx-n5">
       <button type="submit">Register</button>
     </form>
@@ -24,49 +33,92 @@
 </template>
 
 <script>
-import { ref } from 'vue';
 import axios from 'axios';
 
 export default {
   name: 'RegisterOrganization',
-  setup() {
-    const name = ref('');
-    const email = ref('');
-    const phone = ref('');
-    const address = ref('');
-    const password = ref('');
-
-    const registerOrganization = () => {
-      const organization = {
-        name: name.value,
-        email: email.value,
-        phone: phone.value,
-        address: address.value,
-        password: password.value
-      };
-
-      axios.post('http://localhost:8081/organization/add', organization)
-        .then(response => {
-          console.log(response.data);
-          alert("successfully registered");
-          this.organization = {
-        name: '',
-        email: '',
-        phone: '',
-        address: '',
-        password:''
-      };
-
-        })
-        .catch(error => {
-          console.log(error);
-          // handle error
-        });
+  data() {
+    return {
+      name:'',
+      email: '',
+      phone: '',
+      address: '',
+      password: '',
+      errors: {},
+      confirmPassword: '',
+      passwordError: '',
+      errorMessage: ''
     };
+  },
+  methods: {
+    async register() {
+      try {
+        await axios.post('http://localhost:8081/organization/add', {
+          name:this.name,
+          email: this.email,
+          phone: this.phone,
+          address: this.address,
+          password: this.password,
+        });
+        this.$router.push('/signupSuccess');
+      } catch (error) {
+        this.errorMessage = error.response.data.message;
+      }
+    },
+    registerOrganization() {
+      // Clear errors
+      this.errors = {};
+      // Validate form inputs
+      if (!this.validateForm()) {
+        return;
+      }
+      // Send registration data to API endpoint
+      this.register();
+    },
+    validateForm() {
+      this.errors = {};
+      //const ethiopiaCode = '+251';
+      if (!this.email) {
+        this.errors.email = 'Email is required.';
+      }else if (!this.validEmail(this.email)) {
+    this.errors.email = 'Please enter a valid email address.';
+  }
+  if (!this.phone) {
+  this.errors.phone = 'Phone number is required.';
+} else if (!/^(\+251)?(09|07)\d{8}$/.test(this.phone)) {
+  this.errors.phone = 'Invalid phone number format.';
+}
 
-    return { name, email, phone, address, password, registerOrganization };
+      if (!this.address) {
+        this.errors.address = 'Address is required.';
+      }
+      if (!this.password) {
+        this.errors.password = 'Password is required.';
+      }
+      if (!this.confirmPassword) {
+        this.errors.confirmPassword = 'Please confirm your password.';
+      }
+      if (this.password !== this.confirmPassword) {
+        this.passwordError = 'Passwords do not match.';
+        return false; 
+      }
+
+      if (this.password.length < 6) {
+        this.passwordError = 'Password should be at least 6 characters long.';
+        return false;
+      }
+
+      // Return true if there are no errors
+      return Object.keys(this.errors).length === 0;
+    },
+    validEmail(email) {
+  // Regular expression for email validation
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailRegex.test(String(email).toLowerCase());
+}
   }
 };
+
 </script>
 
 <style scoped>
@@ -82,6 +134,7 @@ export default {
 
   /* Form inputs */
   input[type="text"],
+  input[type="tel"],
   input[type="email"],
   input[type="password"] {
     display: block;
@@ -93,7 +146,11 @@ export default {
     border-radius: 5px;
     box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
   }
+ 
+  h1 {
+    color:#3498db;
 
+  }
   /* Form button */
   button[type="submit"] {
     display: block;
@@ -117,8 +174,6 @@ export default {
     font-weight: bold;
     margin-bottom: 10px;
   }
-
-
   /* Media queries for responsiveness */
   @media screen and (max-width: 600px) {
     .form-container {

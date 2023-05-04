eab2ba236d9196d75699d9917c290206760741cd
bereket honelign <bekhonelign@gmail.com>
2023-05-04 20:17:51 +0300

    forgot password

--- src/components/ForgotPassword.vue
+++ src/components/ForgotPassword.vue
@@ -30,6 +30,86 @@
   </script>
   
   <style scoped>
+  .forgot-password {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+  background-image: url("~@/assets/img/background.jpg");
+  background-size: cover;
+  background-position: center center;
+}
+
+.forgot-password__form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+  border-radius: 1rem;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+.forgot-password__title {
+  font-size: 2rem;
+  margin-bottom: 2rem;
+}
+
+.forgot-password__input-container {
+  position: relative;
+  width: 100%;
+  margin-bottom: 2rem;
+}
+
+.forgot-password__input {
+  width: 100%;
+  padding: 0.5rem;
+  font-size: 1rem;
+  border: none;
+  border-bottom: 2px solid #ccc;
+  background-color: transparent;
+}
+
+.forgot-password__input:focus {
+  outline: none;
+  border-bottom-color: #4caf50;
+}
+
+.forgot-password__input-underline {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 2px;
+  width: 0;
+  background-color: #4caf50;
+  transition: width 0.3s;
+}
+
+.forgot-password__input:focus + .forgot-password__input-underline {
+  width: 100%;
+}
+
+.forgot-password__button {
+  background-color: #4caf50;
+  color: white;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.forgot-password__button:hover {
+  background-color: #3e8e41;
+}
+
+@media screen and (max-width: 768px) {
+  .forgot-password__form {
+    width: 90%;
+  }
+}
+
   /* Add your styles here */
   </style>
   

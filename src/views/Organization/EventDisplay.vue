4703e7f7c9534f7b6569606c10d974e03312661e
bereket honelign <bekhonelign@gmail.com>
2023-05-18 13:11:27 +0300

    event display

--- src/views/Organization/EventDisplay.vue
+++ src/views/Organization/EventDisplay.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
-    <event-image />
+    <h1>Event Image</h1>
+    <img v-if="eventImage" :src="eventImage" style="width: 100px; height: 100px" />
+    <p v-else>No image found</p>
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
-import EventImage from './components/EventImage.vue';
+import axios from 'axios';
 
 new Vue({
   el: '#app',
-  components: {
-    // eslint-disable-next-line vue/no-unused-components
-    EventImage,
+  data() {
+    return {
+      eventImage: null,
+    };
+  },
+  mounted() {
+    axios.get('http://localhost:8081/event/2').then((response) => {
+      this.eventImage = response.data;
+    }).catch((error) => {
+      console.log(error);
+    });
   },
 });
 </script>
+

09f28a5a6d28b3d77205cdf944d900ad5d96171f
bereket honelign <bekhonelign@gmail.com>
2023-05-19 22:02:58 +0300

    sample style

--- src/views/Organization/EventDisplay.vue
+++ src/views/Organization/EventDisplay.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-    <div v-for="event in events" :key="event.id">
-      <h1>{{ event.eventName }}</h1>
-      <p>{{ event.eventDescription }}</p>
-      <img :src="getEventImageUrl(event.id)" alt="Event Image" />
+  <div class="event-list">
+    <div v-for="event in events" :key="event.id" class="event-card">
+      <h1 class="event-name">{{ event.eventName }}</h1>
+      <p class="event-description">{{ event.eventDescription }}</p>
+      <p class="event-description">{{ event.ticketPrice }}</p>
+      <p class="event-description">{{ event.location }}</p>
+      <p class="event-description">{{ event.capacity }}</p>
+      <p class="event-description">{{ event.eventDate }}</p>
+      <div class="image-container">
+        <img :src="getEventImageUrl(event.id)" alt="Event Image" class="event-image" />
+      </div>
     </div>
   </div>
 </template>
-
 <script>
 export default {
   data() {
@@ -81,4 +86,43 @@ export default {
 };
 </script>
 
+<style scoped>
+.event-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.event-card {
+  width: 300px;
+  margin: 20px;
+  padding: 20px;
+  background-color: #f4f4f4;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  text-align: center;
+}
+
+.event-name {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.event-description {
+  font-size: 14px;
+  color: #888;
+  margin-bottom: 20px;
+}
+
+.image-container {
+  display: flex;
+  justify-content: center;
+}
+
+.event-image {
+  max-width: 100%;
+  height: auto;
+  border-radius: 4px;
+}
+</style>
 

e5dea9e9d6d49c84c5c556442a21c686a51b441c
bereket honelign <bekhonelign@gmail.com>
2023-05-19 16:04:31 +0300

    new change

--- src/views/Organization/EventTest.vue
+++ src/views/Organization/EventTest.vue
@@ -1,47 +1,58 @@
 <template>
-    <div>
-      <img :src="eventDetails.image" alt="Event Image" />
-      <h2>{{ eventDetails.eventName }}</h2>
-      <p>{{ eventDetails.location }}</p>
-      <p>{{ eventDetails.eventDescription }}</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        eventDetails: {},
-      };
-    },
-    mounted() {
-      // Fetch event details
-      this.fetchEventDetails();
-    },
-    methods: {
-      fetchEventDetails() {
-        const eventId = 2; // Replace with the actual event ID you want to fetch
-        fetch(`http://localhost:8081/event/${eventId}`, {
-          method: 'GET',
+  <div>
+    <img :src="imageUrl" alt="Event Image" />
+    <h2>{{ eventName }}</h2>
+    <p>{{ location }}</p>
+    <p>{{ eventDescription }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageUrl: null,
+      eventName: null,
+      location: null,
+      eventDescription: null,
+    };
+  },
+  mounted() {
+    // Fetch event details
+    this.fetchEventDetails();
+  },
+  methods: {
+    fetchEventDetails() {
+      const eventId = 2; // Replace with the actual event ID you want to fetch
+      fetch(`http://localhost:8081/event/events/${eventId}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+        
+        },
+      })
+        .then(response => {
+          // Check if the response was successful
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error('Failed to fetch event details');
+          }
         })
-          .then(response => {
-            // Check if the response was successful
-            if (response.ok) {
-              // Parse the response as JSON
-              return response.json();
-            } else {
-              throw new Error('Failed to fetch event details');
-            }
-          })
-          .then(data => {
-            // Store the event details in the component data
-            this.eventDetails = data;
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      },
+        .then(event => {
+          // Update the data properties with the received event details
+          this.eventName = event.eventName;
+          this.location = event.location;
+          this.eventDescription = event.eventDescription;
+          
+          // Convert the byte array to an image URL
+          const imageBlob = new Blob([event.image], { type: 'image/png' });
+          this.imageUrl = URL.createObjectURL(imageBlob);
+        })
+        .catch(error => {
+          console.error(error);
+        });
     },
-  };
-  </script>
-  
+  },
+};
+</script>

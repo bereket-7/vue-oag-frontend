3ad0473767f41b8bb923f346cccfa38dd4b81428
bereket honelign <bekhonelign@gmail.com>
2023-05-20 19:42:55 +0300

    style css

--- src/views/Organization/EventRegister.vue
+++ src/views/Organization/EventRegister.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <h2>Create Event</h2>
+   <br>
+  <div class="form-container">
+    <h1>Create Event</h1>
+    <hr class="mx-n3">
+    <br>
     <form @submit="saveEvent" enctype="multipart/form-data">
       <div>
         <label for="eventName">Event Name:</label>
@@ -30,7 +33,9 @@
         <label for="image">Image:</label>
         <input type="file" id="image" ref="fileInput" required />
       </div>
-      <button type="submit">Create Event</button>
+      <br>
+      <hr class="mx-n3">
+      <button type="submit">Submit</button>
     </form>
   </div>
 </template>
@@ -65,6 +70,7 @@ export default {
         .post('http://localhost:8081/event/saveEvent', formData)
         .then((response) => {
           console.log(response.data);
+          this.$router.push('/signupSuccess');
 
           // Add your logic here for handling a successful event creation
         })
@@ -76,3 +82,59 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+
+.form-container {
+  width: 100%;
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+}
+
+input[type="text"],
+input[type="number"] {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 16px;
+}
+
+button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  margin-top: 20px;
+}
+
+button:hover {
+  background-color: #3e8e41;
+}
+label {
+  font-weight:600;
+  text-align: left;
+}
+h1 {
+  color:#3e8e41;
+}
+
+.form-container:hover {
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+}
+
+@media screen and (max-width: 600px) {
+  .form-container {
+    max-width: 100%;
+  }
+}
+
+</style>

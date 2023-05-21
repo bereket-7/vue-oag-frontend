60046c651947c240f08a728312c9f26cf87ade2c
bereket honelign <bekhonelign@gmail.com>
2023-05-21 11:14:11 +0300

    update artwork fetch

--- src/views/Artwork/ArtworkList.vue
+++ src/views/Artwork/ArtworkList.vue
@@ -1,9 +1,8 @@
-
 <template>
   <div class="artwork-gallery">
     <div v-for="artwork in artworks" :key="artwork.id" class="artwork-card">
       <div class="artwork-image">
-        <img :src="getArtworkImageUrl(artwork.id)" alt="Artwork Image" />
+        <img :src="getArtworkImageUrl(artwork.id)" alt="Artwork Image" style="width: 410px; height: 300px;" />
       </div>
       <div class="artwork-details">
         <h3>{{ artwork.artworkName }}</h3>
@@ -11,43 +10,37 @@
         <p>Price: {{ artwork.price }}</p>
         <p>Size: {{ artwork.size }}</p>
         <p>Category: {{ artwork.artworkCategory }}</p>
-        <a href="#" class="btn btn-primary quick-view" data-bs-toggle="modal" data-bs-target="#exampleModal">Quick View</a>
+        <a href="#" class="btn btn-primary quick-view" @click="openModal(artwork)">Quick View</a>
       </div>
     </div>
-  </div>
 
-
-
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Artwork Details</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <div class="row">
-          <div class="col-md-6">
-            <img :src="getArtworkImageUrl(artwork.id)" style="width:410px;height:300px;"  alt="Artwork Image" />
+    <!-- Modal -->
+    <!-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">{{ selectedArtwork.artworkName }}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div class="col-md-6">
-            <h5><b>Name{{ artwork.artworkName }}</b></h5>
-            <hr class="mx-n3">
-            <h5>Description{{ artwork.artworkDescription }}</h5>
-            <h5>Price: {{ artwork.price }}</h5>
-            <h5>Size: {{ artwork.size }}</h5>
-            <h5>Category: {{ artwork.artworkCategory }}</h5>
-            <button type="button" class="btn btn-danger">Add to Cart</button>
+          <div class="modal-body">
+            <div class="artwork-image">
+              <img :src="getArtworkImageUrl(selectedArtwork.id)" alt="Artwork Image" />
+            </div>
+            <div class="artwork-details">
+              <p>{{ selectedArtwork.artworkDescription }}</p>
+              <p>Price: {{ selectedArtwork.price }}</p>
+              <p>Size: {{ selectedArtwork.size }}</p>
+              <p>Category: {{ selectedArtwork.artworkCategory }}</p>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
-</div>
-
-
 </template>
-
 <script>
 import axios from 'axios';
 
@@ -55,6 +48,7 @@ export default {
   data() {
     return {
       artworks: [],
+      selectedArtwork: null,
     };
   },
   mounted() {
@@ -86,7 +80,7 @@ export default {
 }
 
 .artwork-card {
-  width: 250px;
+  width: 400px;
   margin: 20px;
   padding: 10px;
   border: 1px solid #ccc;

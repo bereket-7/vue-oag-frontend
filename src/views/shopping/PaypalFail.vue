303f302cc386d34026a5d93ac1943f80b129c041
bereket <bekhonelign@gmail.com>
2023-05-31 21:23:32 -0700

    failure payment

---
+++ src/views/shopping/PaypalFail.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="failure-page">
+      <h2>Payment Failed!</h2>
+      <p>Oops! Something went wrong with your payment. Please try again later.</p>
+    </div>
+</template>
+  
+  <style scoped>
+  .failure-page {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #f7f7f7;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    text-align: center;
+  }
+  
+  .failure-page h2 {
+    font-size: 24px;
+    margin-bottom: 10px;
+  }
+  
+  .failure-page p {
+    font-size: 16px;
+  }
+  </style>
+  

cfa69f4cae435e3265bd83a22796fe00745f5f17
bereket honelign <bekhonelign@gmail.com>
2023-06-07 16:06:30 +0300

    pagable

--- src/views/Artwork/SearchArtwork.vue
+++ src/views/Artwork/SearchArtwork.vue
@@ -4,6 +4,11 @@
       <ul>
         <li v-for="result in searchResults" :key="result.id">{{ result.artworkName }}</li>
       </ul>
+      <div>
+        <button :disabled="currentPage === 0" @click="previousPage">Previous</button>
+        <span>{{ currentPage + 1 }} / {{ totalPages }}</span>
+        <button :disabled="currentPage === totalPages - 1" @click="nextPage">Next</button>
+      </div>
     </div>
   </template>
   
@@ -15,19 +20,31 @@
       return {
         keyword: '',
         searchResults: [],
+        currentPage: 0,
+        totalPages: 0,
       };
     },
     methods: {
       searchArtwork() {
-        axios.get(`/artworks/autocomplete?keyword=${this.keyword}`)
+        axios.get(`/artworks/search?keyword=${this.keyword}&page=${this.currentPage}`)
           .then((response) => {
             this.searchResults = response.data;
+            this.totalPages = response.headers['x-total-pages'];
           })
           .catch((error) => {
             console.error(error);
           });
       },
+      previousPage() {
+        this.currentPage--;
+        this.searchArtwork();
+      },
+      nextPage() {
+        this.currentPage++;
+        this.searchArtwork();
+      },
     },
   };
   </script>
+  
   

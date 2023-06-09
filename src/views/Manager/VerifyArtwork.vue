3d536b244469ca391962780c00038aeb94704c67
bereket honelign <bekhonelign@gmail.com>
2023-06-09 22:05:57 +0300

    verify artwork

--- src/views/Manager/VerifyArtwork.vue
+++ src/views/Manager/VerifyArtwork.vue
@@ -1,73 +1,112 @@
 <template>
-    <div class="container mt-5">
-      <h1>New Artworks</h1>
-      <table class="table">
-        <thead>
-          <tr>
-            <th>Artwork Name</th>
-            <th>Artwork Photo</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="artwork in artworks" :key="artwork.id">
-            <td>{{ artwork.name }}</td>
-            <td><img :src="artwork.photoUrl" style="max-height: 100px;"></td>
-            <td>
-              <button class="btn btn-success" @click="acceptArtwork(artwork.id)" :disabled="artwork.accepted">Accept</button>
-              <button class="btn btn-danger" @click="rejectArtwork(artwork.id)" :disabled="artwork.rejected">Reject</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+  <div>
+    <div v-for="artwork in artworks" :key="artwork.id" class="artwork-card">
+      <img :src="getArtworkImageUrl(artwork.id)" alt="Artwork Image">
+      <div class="artwork-description">
+        <h3>{{ artwork.description }}</h3>
+      </div>
+      <div class="buttons">
+        <button @click="acceptArtwork(artwork.id)">Accept</button>
+        <button @click="rejectArtwork(artwork.id)">Reject</button>
+      </div>
     </div>
-  </template>
+
+    <div class="popup" :class="{ 'open-popup': showPopup }">
+      <img src="tick.png" alt="tick">
+      <h2>{{ popupMessage }}</h2>
+      <button @click="closePopup()">OK</button>
+    </div>
+  </div>
+</template>
   
-  <script>
-  import axios from 'axios';
   
-  export default {
-    data() {
-      return {
-        artworks: []
-      };
-    },
-    mounted() {
-      this.fetchArtworks();
-    },
-    methods: {
-      fetchArtworks() {
-        axios.get('/api/artworks')
-          .then(response => {
-            this.artworks = response.data;
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      },
-      acceptArtwork(id) {
-        axios.post(`/api/artworks/${id}/accept`)
-        // eslint-disable-next-line no-unused-vars
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      artworks: [],
+      showPopup: false,
+      popupMessage: '',
+    };
+  },
+  mounted() {
+    this.fetchArtworks();
+  },
+  methods: {
+    fetchArtworks() {
+      axios.get('http://localhost:8081/artworks')
         .then(response => {
-            const artwork = this.artworks.find(a => a.id === id);
-            artwork.accepted = true;
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      },
-      rejectArtwork(id) {
-        axios.post(`/api/artworks/${id}/reject`)
-          // eslint-disable-next-line no-unused-vars
-          .then(response => {
-            const artwork = this.artworks.find(a => a.id === id);
-            artwork.rejected = true;
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      }
-    }
-  };
-  </script>
-  
+          this.artworks = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    getArtworkImageUrl(artworkId) {
+      return `http://localhost:8081/artworks/${artworkId}/image`;
+    },
+    acceptArtwork(artworkId) {
+      axios.put(`http://localhost:8081/artworks/${artworkId}/accept`)
+        .then(response => {
+          this.showSuccessPopup(response.data);
+        })
+        .catch(error => {
+          this.showErrorPopup(error.response.data);
+        });
+    },
+    rejectArtwork(artworkId) {
+      axios.put(`http://localhost:8081/artworks/${artworkId}/reject`)
+        .then(response => {
+          this.showSuccessPopup(response.data);
+        })
+        .catch(error => {
+          this.showErrorPopup(error.response.data);
+        });
+    },
+    showSuccessPopup(message) {
+      this.popupMessage = message;
+      this.showPopup = true;
+    },
+    showErrorPopup(message) {
+      this.popupMessage = message;
+      this.showPopup = true;
+    },
+    closePopup() {
+      this.showPopup = false;
+      this.popupMessage = '';
+    },
+  },
+};
+</script>
+
+<style scoped>
+.artwork-card {
+  width: 300px;
+  margin-bottom: 20px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.artwork-card img {
+  width: 100%;
+  height: auto;
+  margin-bottom: 10px;
+}
+
+.artwork-description h3 {
+  margin: 0;
+}
+
+.buttons button {
+  margin-top: 10px;
+  padding: 5px 10px;
+  background-color: #04AA6D;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>

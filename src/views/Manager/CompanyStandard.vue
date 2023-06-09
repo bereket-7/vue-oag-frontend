ee9ddf6e4fd4403ca419a9e09356136e33c0f06d
bereket honelign <bekhonelign@gmail.com>
2023-06-09 20:58:09 +0300

    add standard style

--- src/views/Manager/CompanyStandard.vue
+++ src/views/Manager/CompanyStandard.vue
@@ -1,28 +1,33 @@
 <template>
-    <div class="container" style="margin-top: 100px;">
+    <div class="container">
       <button onclick="document.getElementById('id01').style.display='block'">Add Standard</button>
   
       <div id="id01" class="modal">
-        <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">times;</span>
+        <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal"><i class="fa-solid fa-xmark"></i></span>
         <form class="modal-content" action="/action_page.php">
           <div class="container">
-            <h1>Add Standard</h1>
+            <h1 style="margin-top: 20px;">Add Standard</h1>
             <p>Please enter company standards here </p>
             <hr>
-            <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter Email" name="email" required>
+            <label for="standardDescription"><b>Standard description</b></label>
+            <div class="input-group flex-nowrap py-3">
+              <textarea type="text" class="form-control form-control-lg" id="standardDescription" rows="3" v-model="standardDescription" required></textarea>
+            </div>
   
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
+            <div>
+              <label for="standardType"><b>Standard Type</b></label>
+              <select class="form-select" id="standardType" v-model="standardType" required>
+                <option value="organizational" selected>Organizational</option>
+                <option value="artwork">Artwork</option>
+                <option value="customer">Customer</option>
+              </select>
+            </div>
   
-            <label for="psw-repeat"><b>Repeat Password</b></label>
-            <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-  
-            <p>This will be displayed in the company list of  <a href="#" style="color:dodgerblue">policy & standards.</a>.</p>
+            <p style="margin-top: 60px;">This will be displayed in the company list of <a href="#" style="color:dodgerblue">policy & standards.</a></p>
   
             <div class="clearfix">
-              <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-              <button type="submit" class="signup">Sign Up</button>
+              <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn" style="float: left;">Cancel</button>
+              <button type="submit" class="signup" @click.prevent="saveStandard()" style="float: right;">Submit</button>
             </div>
           </div>
         </form>
@@ -30,6 +35,7 @@
       <footer-view/>
     </div>
   </template>
+  
   
 
 <style scoped>
@@ -50,6 +56,7 @@ input[type=text]:focus, input[type=password]:focus {
   outline: none;
 }
 
+
 button {
   background-color: #04AA6D;
   color: white;
@@ -57,7 +64,7 @@ button {
   margin: 8px 0;
   border: none;
   cursor: pointer;
-  width: 100%;
+  width: 15%;
   opacity: 0.9;
 }
 
@@ -66,17 +73,18 @@ button:hover {
 }
 
 .cancelbtn {
-  padding: 14px 20px;
+  /* padding: 14px 20px; */
   background-color: #f44336;
 }
 
 .cancelbtn, .signupbtn {
   float: left;
-  width: 50%;
+  width: 20%;
 }
 
 .container {
   padding: 16px;
+  margin-top: 120px;
 }
 
 .modal {
@@ -89,7 +97,7 @@ button:hover {
   height: 100%;
   overflow: auto; 
   background-color: #474e5d;
-  padding-top: 50px;
+  padding-top: 100px;
 }
 
 .modal-content {
@@ -125,6 +133,7 @@ hr {
   clear: both;
   display: table;
 }
+
 
 @media screen and (max-width: 300px) {
   .cancelbtn, .signupbtn {

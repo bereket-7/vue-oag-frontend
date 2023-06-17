dea93304f345a694c9d41965f98d24356cb01a29
bereket honelign <bekhonelign@gmail.com>
2023-06-17 20:44:42 +0300

    mail template

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -4,19 +4,19 @@
     <h2>Contact Us</h2>
     <div class="row gy-4">
       <div class="col-lg-6">
-        <form action="forms/contact.php" method="post" class="php-email-form">
+        <form @submit="sendEmail" class="php-email-form">
           <div class="row gy-4">
             <div class="col-md-6">
-              <input type="text" name="name" class="form-control" placeholder="Your Name" required>
+              <input v-model="name" type="text" name="name" class="form-control" placeholder="Your Name" required>
             </div>
             <div class="col-md-6">
-              <input type="email" class="form-control" name="email" placeholder="Your Email" required>
+              <input v-model="email" type="email" class="form-control" name="email" placeholder="Your Email" required>
             </div>
             <div class="col-md-12">
-              <input type="text" class="form-control" name="subject" placeholder="Subject" required>
+              <input v-model="subject" type="text" class="form-control" name="subject" placeholder="Subject" required>
             </div>
             <div class="col-md-12">
-              <textarea class="form-control" name="message" rows="6" placeholder="Message" required></textarea>
+              <textarea v-model="message" class="form-control" name="message" rows="6" placeholder="Message" required></textarea>
             </div>
             <div class="col-md-12 text-center">
               <div class="error-message"></div>

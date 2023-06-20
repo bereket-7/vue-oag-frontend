ef6ea13d503f512ef1f157a6c9e543f67bb2a38f
bereket honelign <bekhonelign@gmail.com>
2023-06-20 07:24:04 +0300

    forgot password style

--- src/components/ForgotPassword.vue
+++ src/components/ForgotPassword.vue
@@ -5,15 +5,14 @@
         <div v-if="!showResetForm" class="forgot-password__input-container">
           <p>Enter your email address to receive verification</p>
           <input type="email" placeholder="Email" v-model="email" class="forgot-password__input" />
-          <div class="forgot-password__input-underline"></div>
           <br><br>
           <button @click="requestPasswordReset" class="forgot-password__button">Submit</button>
         </div>
         <div v-else>
-      <p>Enter your new password:</p>
-      <input type="password" v-model="newPassword" placeholder="New Password">
-      <button @click="resetPassword">Reset Password</button>
-    </div>
+          <p>Enter your new password:</p>
+          <input type="password" v-model="newPassword" placeholder="New Password">
+          <button @click="resetPassword">Reset Password</button>
+        </div>
     <p v-if="message">{{ message }}</p>
       </form>
     </div>
@@ -68,87 +67,96 @@ export default {
 
 
   
-  <style scoped>
+<style scoped>
   .forgot-password {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh; 
-  background-color: #f5f5f5;
-  background-image: url("~@/assets/img/buetyback.png");
-  background-size: cover;
-  background-position: center center;
+  height: 100vh;
 }
 
 .forgot-password__form {
   display: flex;
   flex-direction: column;
-  max-width: 600px;
   align-items: center;
-  padding: 2rem;
-  border-radius: 1rem;
-  background-color: #fff;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  width: 90%;
+  max-width: 400px;
+  padding: 20px;
+  border-radius: 10px;
+  background-color: #ffffff;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
 }
 
 .forgot-password__title {
-  font-size: 2rem;
-  margin-bottom: 2rem;
+  margin-bottom: 20px;
+  font-size: 24px;
+  font-weight: bold;
+  color: #333333;
 }
 
 .forgot-password__input-container {
-  position: relative;
-  width: 100%;
-  margin-bottom: 2rem;
+  margin-bottom: 20px;
 }
 
 .forgot-password__input {
   width: 100%;
-  padding: 0.5rem;
-  font-size: 1rem;
-  border: none;
-  border-bottom: 2px solid #ccc;
-  background-color: transparent;
-}
-
-.forgot-password__input:focus {
+  padding: 12px;
+  border: 1px solid #cccccc;
+  border-radius: 5px;
+  font-size: 14px;
   outline: none;
-  border-bottom-color: #4caf50;
 }
 
 .forgot-password__input-underline {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  height: 2px;
-  width: 0;
-  background-color: #4caf50;
-  transition: width 0.3s;
-}
-
-.forgot-password__input:focus + .forgot-password__input-underline {
   width: 100%;
+  height: 2px;
+  background-color: #007bff;
 }
 
 .forgot-password__button {
-  background-color: #4caf50;
-  color: white;
-  padding: 0.5rem 1rem;
+  width: 100%;
+  padding: 12px;
+  background-color:  #3498db;
+  color: #ffffff;
   border: none;
-  border-radius: 0.5rem;
+  border-radius: 5px;
+  font-size: 16px;
+  font-weight: bold;
   cursor: pointer;
-  transition: background-color 0.3s;
+  transition: background-color 0.3s ease;
 }
 
 .forgot-password__button:hover {
-  background-color: #3e8e41;
+  background-color: #0056b3;
 }
 
-@media screen and (max-width: 768px) {
+.forgot-password__message {
+  margin-top: 10px;
+}
+
+.forgot-password__message p {
+  font-size: 14px;
+  color: #555555;
+}
+
+@media screen and (max-width: 480px) {
   .forgot-password__form {
-    width: 90%;
+    padding: 16px;
+  }
+  
+  .forgot-password__title {
+    font-size: 20px;
+  }
+  
+  .forgot-password__input {
+    padding: 10px;
+    font-size: 12px;
+  }
+  
+  .forgot-password__button {
+    padding: 10px;
+    font-size: 14px;
   }
 }
-
-  </style>
+</style>
   

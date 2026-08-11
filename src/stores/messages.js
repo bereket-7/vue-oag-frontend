95f2af1491a34350cf75e7ce56563d03eed9567f
bereket-7 <bekhonelign@gmail.com>
2026-08-11 16:57:37 +0300

    feat: add messages store

---
+++ src/stores/messages.js
@@ -0,0 +1,37 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import { messageService } from '@/services/messageService';
+
+export const useMessageStore = defineStore('messages', () => {
+  const threads = ref([]);
+  const currentThread = ref([]);
+  const loading = ref(false);
+
+  const fetchThreads = async () => {
+    loading.value = true;
+    try {
+      threads.value = await messageService.getThreads();
+      return threads.value;
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  const fetchThread = async (threadId) => {
+    loading.value = true;
+    try {
+      currentThread.value = await messageService.getThread(threadId);
+      return currentThread.value;
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  const sendMessage = async (threadId, senderId, senderName, body) => {
+    const msg = await messageService.send(threadId, senderId, senderName, body);
+    currentThread.value.push(msg);
+    return msg;
+  };
+
+  return { threads, currentThread, loading, fetchThreads, fetchThread, sendMessage };
+});

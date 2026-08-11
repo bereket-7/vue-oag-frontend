bf7b9c04f896b340174eca50983179bc91ed0af0
bereket-7 <bekhonelign@gmail.com>
2026-08-11 16:57:18 +0300

    update src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
- <UserHeader/>
-  <router-view/>
+  <a href="#main-content" class="sr-only focus:not-sr-only focus:absolute focus:top-2 focus:left-2 focus:z-[100] focus:px-4 focus:py-2 focus:bg-purple-600 focus:text-white focus:rounded-lg">
+    Skip to content
+  </a>
+  <UserHeader />
+  <main id="main-content">
+    <router-view />
+  </main>
   <ToastNotification />
 </template>
 
@@ -11,70 +16,9 @@ import ToastNotification from '@/components/common/ToastNotification.vue';
 
 <style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
-
-<!-- <template>
-  <component :is="navbarComponent" />
-  <router-view />
-</template>
-
-<script>
-import UserHeader from '@/views/User/UserHeader.vue'
-import ManagerHeader from '@/views/Manager/ManagerHeader.vue'
-import CustomerHeader from '@/views/Customer/CustomerHeader.vue'
-import AdminHeader from '@/views/Admin/AdminHeader.vue'
-
-export default {
-  name: 'App',
-  components: {
-    UserHeader,
-    ManagerHeader,
-    CustomerHeader,
-    AdminHeader
-  },
-  data() {
-    return {
-      navbarComponent: 'UserHeader' // Default to UserHeader component
-    };
-  },
-  created() {
-    // Get user role after login (you need to implement your own logic here)
-    const userRole = 'manager'; // Replace with the actual user role
-
-    // Update navbarComponent based on user role
-    switch (userRole) {
-      case 'manager':
-        this.navbarComponent = 'ManagerHeader';
-        break;
-      case 'customer':
-        this.navbarComponent = 'CustomerHeader';
-        break;
-      case 'admin':
-        this.navbarComponent = 'AdminHeader';
-        break;
-      default:
-        this.navbarComponent = 'UserHeader';
-    }
-  }
-};
-</script>
- -->

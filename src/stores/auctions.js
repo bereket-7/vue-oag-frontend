4358ef019e461923967b836bb911da818c6b7fe5
bereket-7 <bekhonelign@gmail.com>
2026-08-11 16:57:36 +0300

    feat: add auctions store

---
+++ src/stores/auctions.js
@@ -0,0 +1,52 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import { auctionService } from '@/services/auctionService';
+
+export const useAuctionStore = defineStore('auctions', () => {
+  const auctions = ref([]);
+  const currentAuction = ref(null);
+  const loading = ref(false);
+
+  const fetchAuctions = async () => {
+    loading.value = true;
+    try {
+      auctions.value = await auctionService.getAll();
+      return auctions.value;
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  const fetchAuctionById = async (id) => {
+    loading.value = true;
+    try {
+      currentAuction.value = await auctionService.getById(id);
+      return currentAuction.value;
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  const createAuction = async (data) => {
+    const auction = await auctionService.create(data);
+    auctions.value.unshift(auction);
+    return auction;
+  };
+
+  const placeBid = async (auctionId, userId, userName, amount) => {
+    currentAuction.value = await auctionService.placeBid(auctionId, userId, userName, amount);
+    const idx = auctions.value.findIndex((a) => a.id === auctionId);
+    if (idx !== -1) auctions.value[idx] = currentAuction.value;
+    return currentAuction.value;
+  };
+
+  const watchAuction = async (auctionId, userId) => {
+    return auctionService.watch(auctionId, userId);
+  };
+
+  const unwatchAuction = async (auctionId, userId) => {
+    return auctionService.unwatch(auctionId, userId);
+  };
+
+  return { auctions, currentAuction, loading, fetchAuctions, fetchAuctionById, createAuction, placeBid, watchAuction, unwatchAuction };
+});

7b5a7e62b6c326ccc9f8cea613defd0780a74fc9
bereket-7 <bekhonelign@gmail.com>
2026-08-11 16:57:18 +0300

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
+  env: { node: true },
+  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended'],
+  parserOptions: { parser: '@babel/eslint-parser' },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    'vue/multi-word-component-names': 'off'
   }
-}
+};

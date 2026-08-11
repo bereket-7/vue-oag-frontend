7aeb7d77be84a882463848fd69459923967fab6a
bereket-7 <bekhonelign@gmail.com>
2026-08-11 16:57:35 +0300

    update src/main.js

--- src/main.js
+++ src/main.js
@@ -2,16 +2,19 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
+import i18n from './i18n';
 import './assets/tailwind.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
-import "carousel";
 import './assets/style.css';
 import '@fortawesome/fontawesome-free/js/all';
+
+if (localStorage.getItem('theme') === 'dark') {
+  document.documentElement.classList.add('dark');
+}
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
+app.use(i18n);
 app.mount('#app');

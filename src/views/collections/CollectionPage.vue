48db039adc53b003f27e052e27c62a12832a0ea9
bereket-7 <bekhonelign@gmail.com>
2026-08-14 16:44:40 +0300

    style: format CollectionPage markup to satisfy Vue ESLint template rules

--- src/views/collections/CollectionPage.vue
+++ src/views/collections/CollectionPage.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="min-h-screen bg-gray-50 pt-20">
     <PageLoader v-if="loading" />
-    <div v-else-if="collection" class="container mx-auto px-4 py-8">
-      <h1 class="text-3xl font-bold mb-2">{{ collection.title }}</h1>
-      <p class="text-gray-600 mb-8">{{ collection.description }}</p>
+    <div
+      v-else-if="collection"
+      class="container mx-auto px-4 py-8"
+    >
+      <h1 class="text-3xl font-bold mb-2">
+        {{ collection.title }}
+      </h1>
+      <p class="text-gray-600 mb-8">
+        {{ collection.description }}
+      </p>
       <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        <ArtworkCard v-for="item in collection.artworks" :key="item.id" :artwork="item" show-actions @view-details="$router.push(`/artworks/${item.id}`)" />
+        <ArtworkCard
+          v-for="item in collection.artworks"
+          :key="item.id"
+          :artwork="item"
+          show-actions
+          @view-details="$router.push(`/artworks/${item.id}`)"
+        />
       </div>
     </div>
   </div>

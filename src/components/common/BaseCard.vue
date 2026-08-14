3852ec267ae4c3b487a70fa0f00302de7553cac2
bereket-7 <bekhonelign@gmail.com>
2026-08-14 16:44:37 +0300

    style: format BaseCard markup to satisfy Vue ESLint template rules

--- src/components/common/BaseCard.vue
+++ src/components/common/BaseCard.vue
@@ -1,8 +1,13 @@
 <template>
   <div :class="cardClasses">
-    <div v-if="$slots.header || title" class="px-6 py-4 border-b border-gray-200">
+    <div
+      v-if="$slots.header || title"
+      class="px-6 py-4 border-b border-gray-200"
+    >
       <slot name="header">
-        <h3 class="text-lg font-semibold text-gray-900">{{ title }}</h3>
+        <h3 class="text-lg font-semibold text-gray-900">
+          {{ title }}
+        </h3>
       </slot>
     </div>
     
@@ -10,7 +15,10 @@
       <slot />
     </div>
     
-    <div v-if="$slots.footer" class="px-6 py-4 border-t border-gray-200 bg-gray-50">
+    <div
+      v-if="$slots.footer"
+      class="px-6 py-4 border-t border-gray-200 bg-gray-50"
+    >
       <slot name="footer" />
     </div>
   </div>

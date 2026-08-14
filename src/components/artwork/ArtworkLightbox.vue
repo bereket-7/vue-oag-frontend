0ade8004d45d7c6afbb9034874e605beda932300
bereket-7 <bekhonelign@gmail.com>
2026-08-14 16:44:37 +0300

    style: format ArtworkLightbox markup to satisfy Vue ESLint template rules

--- src/components/artwork/ArtworkLightbox.vue
+++ src/components/artwork/ArtworkLightbox.vue
@@ -1,7 +1,22 @@
 <template>
-  <div class="fixed inset-0 z-50 bg-black/90 flex items-center justify-center p-4" @click.self="$emit('close')" @keydown.esc="$emit('close')" tabindex="0">
-    <button class="absolute top-4 right-4 text-white text-2xl" @click="$emit('close')" aria-label="Close"><i class="fas fa-times"></i></button>
-    <img :src="src" :alt="alt" class="max-w-full max-h-[90vh] object-contain" />
+  <div
+    class="fixed inset-0 z-50 bg-black/90 flex items-center justify-center p-4"
+    tabindex="0"
+    @click.self="$emit('close')"
+    @keydown.esc="$emit('close')"
+  >
+    <button
+      class="absolute top-4 right-4 text-white text-2xl"
+      aria-label="Close"
+      @click="$emit('close')"
+    >
+      <i class="fas fa-times" />
+    </button>
+    <img
+      :src="src"
+      :alt="alt"
+      class="max-w-full max-h-[90vh] object-contain"
+    >
   </div>
 </template>
 

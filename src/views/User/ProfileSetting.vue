3df5d0bfe4a6290ab8c477d0cf820a32ed9e63bb
bereket-7 <bekhonelign@gmail.com>
2026-08-14 16:44:39 +0300

    style: format ProfileSetting markup to satisfy Vue ESLint template rules

--- src/views/User/ProfileSetting.vue
+++ src/views/User/ProfileSetting.vue
@@ -1,33 +1,77 @@
 <template>
-    <div class="container-fluid rounded bg-white mt-5">
-        <div class="row">
-            <div class="col-md-3 border-right sidebar">
-                <div class="d-flex flex-column align-items-center text-center p-3  user-p">
-                   <ProfilePicture />
-                </div>
-                <div class="sidebar">
-                </div>
-            </div>
-            <div class="col-md-8 border-right">
-                <div class="p-3 py-5">
-                    <div class="d-flex justify-content-between align-items-center mb-3">
-                        <h4 class="text-right">Profile Settings</h4>
-                    </div>
-                    <div class="row mt-2">
-                        <div class="col-md-6"><label class="labels">First Name</label><input type="text" class="form-control" placeholder="first name" value="" required></div>
-                        <div class="col-md-6"><label class="labels">Last Name</label><input type="text" class="form-control" value="" placeholder="last name"></div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-md-12"><label class="labels">Enter Old Password</label><input type="text" class="form-control" placeholder="enter old password" value=""></div>
-                        <div class="col-md-12"><label class="labels">Enter New Password</label><input type="text" class="form-control" placeholder="Enter New Password" value=""></div>
-                        <div class="col-md-12"><label class="labels">Confirm New Password</label><input type="text" class="form-control" placeholder="Confirm New Password" value=""></div>
-                    </div>
-                    <div class="mt-5 text-center"><button class="btn btn-primary profile-button " type="button">Save Profile</button></div>
-                </div>
-            </div>
+  <div class="container-fluid rounded bg-white mt-5">
+    <div class="row">
+      <div class="col-md-3 border-right sidebar">
+        <div class="d-flex flex-column align-items-center text-center p-3  user-p">
+          <ProfilePicture />
         </div>
+        <div class="sidebar" />
+      </div>
+      <div class="col-md-8 border-right">
+        <div class="p-3 py-5">
+          <div class="d-flex justify-content-between align-items-center mb-3">
+            <h4 class="text-right">
+              Profile Settings
+            </h4>
+          </div>
+          <div class="row mt-2">
+            <div class="col-md-6">
+              <label class="labels">First Name</label><input
+                type="text"
+                class="form-control"
+                placeholder="first name"
+                value=""
+                required
+              >
+            </div>
+            <div class="col-md-6">
+              <label class="labels">Last Name</label><input
+                type="text"
+                class="form-control"
+                value=""
+                placeholder="last name"
+              >
+            </div>
+          </div>
+          <div class="row mt-3">
+            <div class="col-md-12">
+              <label class="labels">Enter Old Password</label><input
+                type="text"
+                class="form-control"
+                placeholder="enter old password"
+                value=""
+              >
+            </div>
+            <div class="col-md-12">
+              <label class="labels">Enter New Password</label><input
+                type="text"
+                class="form-control"
+                placeholder="Enter New Password"
+                value=""
+              >
+            </div>
+            <div class="col-md-12">
+              <label class="labels">Confirm New Password</label><input
+                type="text"
+                class="form-control"
+                placeholder="Confirm New Password"
+                value=""
+              >
+            </div>
+          </div>
+          <div class="mt-5 text-center">
+            <button
+              class="btn btn-primary profile-button "
+              type="button"
+            >
+              Save Profile
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-    </template>
+  </div>
+</template>
     
     <script>
     import ProfilePicture from "@/components/ProfilePicture.vue";

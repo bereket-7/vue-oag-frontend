baae99e3c4a550e995974e2d03837a5fa4bde58a
bereket-7 <bekhonelign@gmail.com>
2026-08-14 16:44:37 +0300

    style: format BidHistory markup to satisfy Vue ESLint template rules

--- src/components/auctions/BidHistory.vue
+++ src/components/auctions/BidHistory.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="bg-white rounded-lg border p-4">
-    <h3 class="font-semibold mb-3">Bid History</h3>
-    <div v-if="bids?.length" class="space-y-2 max-h-48 overflow-y-auto">
-      <div v-for="bid in bids" :key="bid.id" class="flex justify-between text-sm">
+    <h3 class="font-semibold mb-3">
+      Bid History
+    </h3>
+    <div
+      v-if="bids?.length"
+      class="space-y-2 max-h-48 overflow-y-auto"
+    >
+      <div
+        v-for="bid in bids"
+        :key="bid.id"
+        class="flex justify-between text-sm"
+      >
         <span>{{ bid.userName }}</span>
         <span class="font-medium">{{ formatPrice(bid.amount) }}</span>
       </div>
     </div>
-    <p v-else class="text-gray-400 text-sm">No bids yet</p>
+    <p
+      v-else
+      class="text-gray-400 text-sm"
+    >
+      No bids yet
+    </p>
   </div>
 </template>
 

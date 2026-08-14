4ba5cfe6df8f4bf4546d547960e25434b7b6b980
bereket-7 <bekhonelign@gmail.com>
2026-08-14 16:44:40 +0300

    style: format OrderSummary markup to satisfy Vue ESLint template rules

--- src/views/shopping/checkout/OrderSummary.vue
+++ src/views/shopping/checkout/OrderSummary.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="bg-gray-50 rounded-lg p-4 space-y-2 text-sm">
-    <div class="flex justify-between"><span>Subtotal</span><span>{{ formatPrice(subtotal) }}</span></div>
-    <div class="flex justify-between"><span>Shipping</span><span>{{ formatPrice(shipping) }}</span></div>
-    <div class="flex justify-between"><span>Tax</span><span>{{ formatPrice(tax) }}</span></div>
-    <div class="flex justify-between font-bold text-lg pt-2 border-t"><span>Total</span><span>{{ formatPrice(total) }}</span></div>
+    <div class="flex justify-between">
+      <span>Subtotal</span><span>{{ formatPrice(subtotal) }}</span>
+    </div>
+    <div class="flex justify-between">
+      <span>Shipping</span><span>{{ formatPrice(shipping) }}</span>
+    </div>
+    <div class="flex justify-between">
+      <span>Tax</span><span>{{ formatPrice(tax) }}</span>
+    </div>
+    <div class="flex justify-between font-bold text-lg pt-2 border-t">
+      <span>Total</span><span>{{ formatPrice(total) }}</span>
+    </div>
   </div>
 </template>
 

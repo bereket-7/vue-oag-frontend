02012815d70ef8d8e150107e0d424b0fe0c67c6e
bereket-7 <bekhonelign@gmail.com>
2026-08-14 16:44:39 +0300

    style: format CustomerDashBoard markup to satisfy Vue ESLint template rules

--- src/views/User/CustomerDashBoard.vue
+++ src/views/User/CustomerDashBoard.vue
@@ -1,77 +1,183 @@
 <template>
-    <div class="container-fluid rounded bg-white">
-      <div class="justify-content-between align-items-center border-bottom border-top dashboard">
-                        <h2 class="text-start font-weight-bold">MY ACCOUNT</h2>
-                        <h6 class="text-start">DASHBOARD</h6>
-                    </div>
-        <div class="row">
-            <div class="col-md-3 sidebar">
-                <div class="d-flex flex-column align-items-center text-center p-3  user-p">
-                    <img class="rounded-circle mt-5" width="150px" src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=300&q=80">
-                    <span class="font-weight-bold">Bereket</span>
-                </div>
-                <div class="sidebar">
-                    <ul class="navbar-list">
-        <li class="nav-item">
-          <router-link to="/" class="nav-link border-bottom text-start sideNavList">HOME</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/about" class="nav-link border-bottom  text-start sideNavList">ABOUT</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/contactUs" class="nav-link border-bottom text-start sideNavList">CONTACT US</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/adminPanel" class="nav-link border-bottom text-start sideNavList">ADMIN</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/adminPanel" class="nav-link border-bottom text-start sideNavList">ACCOUNT DETAIL</router-link>
-        </li>
-      
-        <li class="nav-item">  
-    <button @click="showConfirmationDialog = true">Logout</button> 
-    <div v-if="showConfirmationDialog" class="confirmation-dialog">
-      <p>Are you sure you want to logout?</p>
-      <button @click="logoutUser">Yes</button>
-      <button @click="cancelLogout">No</button>
+  <div class="container-fluid rounded bg-white">
+    <div class="justify-content-between align-items-center border-bottom border-top dashboard">
+      <h2 class="text-start font-weight-bold">
+        MY ACCOUNT
+      </h2>
+      <h6 class="text-start">
+        DASHBOARD
+      </h6>
     </div>
- </li>
-      </ul> 
-
-                  </div>
-                    
-                </div>
-  
-            <div class="col-md-8 border-right">
-              
-                <div class="p-3 py-5">
-                  <div class="dashboardText">
-                <p>The user dashboard on an online art gallery allows you to easily navigate, discover, and engage with artworks, artists, and personalized features.</p>
-              </div>
-                    <div class="row">
-                        <div class="row mt-2">
-                        <div class="col-md-4"><router-link to="/"><button type="button" class="btn btn-outline-secondary w-100 h-1 m-1 text" style="height:70px;">Home</button></router-link></div>
-                        <div class="col-md-4"><router-link to="/about"><button type="button" class="btn btn-outline-secondary w-100 h-1 m-1 text" style="height:70px;">About</button></router-link></div>
-                        <div class="col-md-4"><router-link to="/contactUs"><button type="button" class="btn btn-outline-secondary w-100 h-1 m-1 text" style="height:70px;">Contact Us</button></router-link></div>
-                         
-                    </div>
-                    <div class="row mt-2">
-                        <div class="col-md-4"><router-link to="/artworkList"><button type="button" class="btn btn-outline-secondary w-100 h-1 m-1 text" style="height:70px;">Paintings</button></router-link></div>
-                        <div class="col-md-4"><router-link to="/customerDashboard"><button type="button" class="btn btn-outline-secondary w-100 h-1 m-1 text" style="height:70px;">My Account</button></router-link></div>
-                        <div class="col-md-4"><router-link to="/edit-account"><button type="button" class="btn btn-outline-secondary w-100 h-10 m-1 text" style="height:70px;">Account Details</button></router-link></div>  
-                    </div>
-                    <div class="row mt-2">
-                        <div class="col-md-4"><router-link to=""><button type="button" class="btn btn-outline-secondary w-100 h-10 m-1 text" style="height:70px;">Wishlist</button></router-link></div>
-                    </div>
-
-                    </div>
-
-                </div>
-            </div>
+    <div class="row">
+      <div class="col-md-3 sidebar">
+        <div class="d-flex flex-column align-items-center text-center p-3  user-p">
+          <img
+            class="rounded-circle mt-5"
+            width="150px"
+            src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=300&q=80"
+          >
+          <span class="font-weight-bold">Bereket</span>
         </div>
+        <div class="sidebar">
+          <ul class="navbar-list">
+            <li class="nav-item">
+              <router-link
+                to="/"
+                class="nav-link border-bottom text-start sideNavList"
+              >
+                HOME
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link
+                to="/about"
+                class="nav-link border-bottom  text-start sideNavList"
+              >
+                ABOUT
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link
+                to="/contactUs"
+                class="nav-link border-bottom text-start sideNavList"
+              >
+                CONTACT US
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link
+                to="/adminPanel"
+                class="nav-link border-bottom text-start sideNavList"
+              >
+                ADMIN
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link
+                to="/adminPanel"
+                class="nav-link border-bottom text-start sideNavList"
+              >
+                ACCOUNT DETAIL
+              </router-link>
+            </li>
+      
+            <li class="nav-item">  
+              <button @click="showConfirmationDialog = true">
+                Logout
+              </button> 
+              <div
+                v-if="showConfirmationDialog"
+                class="confirmation-dialog"
+              >
+                <p>Are you sure you want to logout?</p>
+                <button @click="logoutUser">
+                  Yes
+                </button>
+                <button @click="cancelLogout">
+                  No
+                </button>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+  
+      <div class="col-md-8 border-right">
+        <div class="p-3 py-5">
+          <div class="dashboardText">
+            <p>The user dashboard on an online art gallery allows you to easily navigate, discover, and engage with artworks, artists, and personalized features.</p>
+          </div>
+          <div class="row">
+            <div class="row mt-2">
+              <div class="col-md-4">
+                <router-link to="/">
+                  <button
+                    type="button"
+                    class="btn btn-outline-secondary w-100 h-1 m-1 text"
+                    style="height:70px;"
+                  >
+                    Home
+                  </button>
+                </router-link>
+              </div>
+              <div class="col-md-4">
+                <router-link to="/about">
+                  <button
+                    type="button"
+                    class="btn btn-outline-secondary w-100 h-1 m-1 text"
+                    style="height:70px;"
+                  >
+                    About
+                  </button>
+                </router-link>
+              </div>
+              <div class="col-md-4">
+                <router-link to="/contactUs">
+                  <button
+                    type="button"
+                    class="btn btn-outline-secondary w-100 h-1 m-1 text"
+                    style="height:70px;"
+                  >
+                    Contact Us
+                  </button>
+                </router-link>
+              </div>
+            </div>
+            <div class="row mt-2">
+              <div class="col-md-4">
+                <router-link to="/artworkList">
+                  <button
+                    type="button"
+                    class="btn btn-outline-secondary w-100 h-1 m-1 text"
+                    style="height:70px;"
+                  >
+                    Paintings
+                  </button>
+                </router-link>
+              </div>
+              <div class="col-md-4">
+                <router-link to="/customerDashboard">
+                  <button
+                    type="button"
+                    class="btn btn-outline-secondary w-100 h-1 m-1 text"
+                    style="height:70px;"
+                  >
+                    My Account
+                  </button>
+                </router-link>
+              </div>
+              <div class="col-md-4">
+                <router-link to="/edit-account">
+                  <button
+                    type="button"
+                    class="btn btn-outline-secondary w-100 h-10 m-1 text"
+                    style="height:70px;"
+                  >
+                    Account Details
+                  </button>
+                </router-link>
+              </div>  
+            </div>
+            <div class="row mt-2">
+              <div class="col-md-4">
+                <router-link to="">
+                  <button
+                    type="button"
+                    class="btn btn-outline-secondary w-100 h-10 m-1 text"
+                    style="height:70px;"
+                  >
+                    Wishlist
+                  </button>
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <FooterView/>
-    </template>
+  </div>
+  <FooterView />
+</template>
     
     <script>
       import axios from 'axios';

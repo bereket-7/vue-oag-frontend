104f57e90b9293edb9dfdfce0ae6d38322d858e0
bereket-7 <bekhonelign@gmail.com>
2026-08-14 16:44:40 +0300

    style: format Thread markup to satisfy Vue ESLint template rules

--- src/views/messages/Thread.vue
+++ src/views/messages/Thread.vue
@@ -1,17 +1,38 @@
 <template>
   <div class="min-h-screen bg-gray-50 pt-20">
     <div class="container mx-auto px-4 py-8 max-w-2xl">
-      <router-link to="/messages" class="text-purple-600 text-sm mb-4 inline-block"><i class="fas fa-arrow-left mr-1"></i> Inbox</router-link>
+      <router-link
+        to="/messages"
+        class="text-purple-600 text-sm mb-4 inline-block"
+      >
+        <i class="fas fa-arrow-left mr-1" /> Inbox
+      </router-link>
       <div class="bg-white rounded-xl shadow-md p-6 min-h-[400px] flex flex-col">
         <div class="flex-1 space-y-4 mb-4 overflow-y-auto">
-          <div v-for="msg in messages" :key="msg.id" :class="['max-w-[80%] p-3 rounded-lg text-sm', msg.senderId === authStore.user?.id ? 'ml-auto bg-purple-600 text-white' : 'bg-gray-100']">
-            <p class="font-medium text-xs mb-1 opacity-75">{{ msg.senderName }}</p>
+          <div
+            v-for="msg in messages"
+            :key="msg.id"
+            :class="['max-w-[80%] p-3 rounded-lg text-sm', msg.senderId === authStore.user?.id ? 'ml-auto bg-purple-600 text-white' : 'bg-gray-100']"
+          >
+            <p class="font-medium text-xs mb-1 opacity-75">
+              {{ msg.senderName }}
+            </p>
             {{ msg.body }}
           </div>
         </div>
-        <form @submit.prevent="send" class="flex gap-2">
-          <input v-model="body" class="flex-1 border rounded-lg px-4 py-2" placeholder="Type a message..." required />
-          <BaseButton type="submit">Send</BaseButton>
+        <form
+          class="flex gap-2"
+          @submit.prevent="send"
+        >
+          <input
+            v-model="body"
+            class="flex-1 border rounded-lg px-4 py-2"
+            placeholder="Type a message..."
+            required
+          >
+          <BaseButton type="submit">
+            Send
+          </BaseButton>
         </form>
       </div>
     </div>
